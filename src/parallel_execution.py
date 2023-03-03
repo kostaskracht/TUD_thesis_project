0443a5578506c3ba2b5c9d2427f18c0d8f29b238
kostaskracht <kkrachtop@gmail.com>
2023-03-03 01:01:26 +0100

    Minor updates in OLS.

--- src/parallel_execution.py
+++ src/parallel_execution.py
@@ -282,10 +282,10 @@ class MindmapPPOMultithread(MindmapPPO):
                             self.log_after_test_episode(np.mean(test_rewards), episode)
 
                 # CRITIC ERROR
-                if np.sqrt(np.abs(critic_loss.detach().numpy()/returns)) < 0.01:
-                    print(f"STOPPING EXECUTION DUE TO CONVERGENCE OF RETURNS AND VALUES IN EPISODE {episode}.")
-                    print(f"Critic loss is {critic_loss.detach().numpy()} and returns are {returns}.")
-                    break
+                # if np.sqrt(np.abs(critic_loss.detach().numpy()/returns)) < 0.01:
+                #     print(f"STOPPING EXECUTION DUE TO CONVERGENCE OF RETURNS AND VALUES IN EPISODE {episode}.")
+                #     print(f"Critic loss is {critic_loss.detach().numpy()} and returns are {returns}.")
+                #     break
 
         elif exec_mode == "test":
             self._load_model_weights(checkpoint_dir, checkpoint_ep, reuse_mode)
